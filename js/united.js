74bf4ad1d539c9c286913e657c640fc8c10544d7
wangyuhui727 <wangyuhui727@163.com>
2019-08-19 15:51:49 +0800

    1、dialog.js封装为移动端和PC端两套； 2、工作台页面优化； 3、增加mockjs示例

--- js/united.js
+++ js/united.js
@@ -6,17 +6,19 @@
 	var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
 	var path = projectPath + projectName;
 	var version = Math.round(Math.random() * 10); //版本号，更新版本时实时更新，避免缓存问题，此处取随机数
-
+	let mobile = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+	
+	//常用
+	document.write("<script language=javascript src='" + path + "/js/public.js?verson=" + version + "'></script>");
 	//标签
 	document.write("<script language=javascript src='" + path + "/js/tabbar.min.js?verson=" + version + "'></script>");
 	//弹出框
-	document.write("<script language=javascript src='" + path + "/js/dialog.min.js?verson=" + version + "'></script>");
+	if (mobile) document.write("<script language=javascript src='" + path + "/js/dialog.min.js?verson=" + version + "'></script>");
+	else document.write("<script language=javascript src='" + path + "/js/dialogPX.min.js?verson=" + version + "'></script>");
 	//轮播图
 	document.write("<script language=javascript src='" + path + "/js/swiper.min.js?verson=" + version + "'></script>");
 	//动态效果
 	document.write("<script language=javascript src='" + path + "/js/aos.min.js?verson=" + version + "'></script>");
-	//常用
-	document.write("<script language=javascript src='" + path + "/js/public.js?verson=" + version + "'></script>");
 	//vconsole
 	// document.write("<script language=javascript src='"+ path + "/js/vconsole.js?verson=" + version + "'></script>");
 })()
